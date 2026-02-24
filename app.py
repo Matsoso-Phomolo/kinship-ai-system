5435bfbd0df0872fea009c0d58cbdd713a854820
Matsoso-Phomolo <phomolomatsoso@gmail.com>
2026-02-24 22:13:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,29 @@
-from flask import Flask, render_template, request
-from pyswip import Prolog
-import re
-
-app = Flask(__name__)
-
-# Load Prolog knowledge base
-prolog = Prolog()
-prolog.consult("familytree.pl")
-
-
-def extract_name(question):
-    words = question.lower().split()
-    return words[-1].replace("?", "")
-
-
 def process_question(question):
     question = question.lower()
 
-    if "father of" in question:
-        person = extract_name(question)
-        query = f"father_of(X, {person})"
-        result = list(prolog.query(query))
-        if result:
-            name = result[0]["X"]
-            return f"{name.capitalize()} is {person.capitalize()}'s father."
-        else:
-            return "I don't know the father."
+    relationships = {
+        "father of": ("father_of", "father"),
+        "mother of": ("mother_of", "mother"),
+        "brother of": ("brother_of", "brother"),
+        "sister of": ("sister_of", "sister"),
+        "grandfather of": ("grandfather_of", "grandfather"),
+        "grandmother of": ("grandmother_of", "grandmother"),
+        "aunt of": ("aunt_of", "aunt"),
+        "uncle of": ("uncle_of", "uncle"),
+        "ancestor of": ("ancestor_of", "ancestor")
+    }
 
-    elif "mother of" in question:
-        person = extract_name(question)
-        query = f"mother_of(X, {person})"
-        result = list(prolog.query(query))
-        if result:
-            name = result[0]["X"]
-            return f"{name.capitalize()} is {person.capitalize()}'s mother."
-        else:
-            return "I don't know the mother."
+    for phrase, (predicate, label) in relationships.items():
+        if phrase in question:
+            person = extract_name(question)
+            query = f"{predicate}(X, {person})"
+            result = list(prolog.query(query))
 
-    elif "brother of" in question:
-        person = extract_name(question)
-        query = f"brother_of(X, {person})"
-        result = list(prolog.query(query))
-        if result:
-            name = result[0]["X"]
-            return f"{name.capitalize()} is {person.capitalize()}'s brother."
-        else:
-            return "No brother found."
+            if result:
+                answers = [r["X"].capitalize() for r in result]
+                answer_text = ", ".join(answers)
+                return f"{answer_text} is {person.capitalize()}'s {label}."
+            else:
+                return f"No {label} found for {person.capitalize()}."
 
-    else:
-        return "Sorry, I don't understand the question."
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    answer = ""
-    if request.method == "POST":
-        question = request.form["question"]
-        answer = process_question(question)
-    return render_template("index.html", answer=answer)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return "Sorry, I don't understand the question."
